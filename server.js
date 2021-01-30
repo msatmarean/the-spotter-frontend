cfb27e7d4c5502b88b3b8d5cc3f10eb5f5526baa
Marius Satmarean <marius.satmarean@gmail.com>
2021-01-30 14:24:08 +0200

    commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/the-spotter'));
 
 app.get('/*', function (req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/the-spotter/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
